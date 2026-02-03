58eae37cdc732d113fe2aaa1855293894cc09ac0
Nathan <nathansbarros2005@gmail.com>
2026-02-03 11:49:52 -0300

    feat(backend): configure database connection with TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +(configService.get<string>('DB_PORT') ?? 5432),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true, // Ajuda o TypeORM a achar as tabelas automaticamente
+        synchronize: true, // APENAS EM DEV: cria as tabelas automaticamente no banco
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
